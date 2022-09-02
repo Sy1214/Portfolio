85f24bbc027e24acf190e63b4688b0e08121983b
Sy1214 <xdsp4800.nwdw202@gmail.com>
2022-09-02 10:54:54 +0900

    0902

---
+++ js/fadeIn.js
@@ -0,0 +1,50 @@
+$(window).scroll(function () {
+	fadeAnime();	
+	delayFadeAnime();
+});
+
+// 自己紹介アニメーション
+function fadeAnime() {
+	$('.fadeUpTrigger').each(function () {
+		var elm = $(this).offset().top - 30;
+		var scroll = $(window).scrollTop();
+		var height = $(window).height();
+		if (scroll >= elm - height) {
+			$(this).addClass('fadeUp');
+		}
+	});
+}
+
+// 製作事例アニメーション
+function delayFadeAnime() {
+	var time = 0.15;
+	var val = time;
+	$('.delayScroll').each(function () {
+	  var parent = this;
+	  var elm = $(this).offset().top;
+	  var scroll = $(window).scrollTop();
+	  var height = $(window).height();
+	  var childs = $(this).children();
+	  
+	  if (scroll >= elm - height && !$(parent).hasClass("play")) {
+		 $(childs).each(function () {
+			
+			if (!$(this).hasClass("fadeUpInOrder")) {
+			  $(parent).addClass("play");
+			  $(this).css("animation-delay", val + "s");
+			  $(this).addClass("fadeUpInOrder");
+			  val = val + time;
+			  
+			  var index = $(childs).index(this);
+			  if((childs.length-1) == index){
+				 $(parent).removeClass("play");
+			  }
+			}
+		 })
+	  }else {
+		 $(childs).removeClass("fadeUpInOrder");
+		 val = time;
+	  }
+	})
+ }
+ 
---
+++ js/textTyping.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+	var elm = $(".typeTextAnime");
+	elm.each(function () {
+		var txt = $(this).html();
+		var txtbox = "";
+		txt.split('').forEach(function (n) {
+			if (n !== " ") {
+				txtbox += '<span>' + n + '</span>';
+			} else {
+				txtbox += n;
+			}
+		});
+		$(this).html(txtbox);
+	});
+	typeTxt();
+});
+
+function typeTxt() {
+	$('.typeTextAnime').each(function () {
+		var thisChild = "";
+		thisChild = $(this).children();
+		thisChild.each(function (i) {
+			var time = 80;
+			$(this).delay(time * i).fadeIn(time);
+		});
+	});
+}
